5b576b809806ba1fb265da4c5f3fefa2228fedec
Marek <marekgacek45@gmail.com>
2024-06-04 12:20:52 +0200

    install tailwind

---
+++ app/Filament/Resources/ApartmentResource.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ApartmentResource\Pages;
+use App\Filament\Resources\ApartmentResource\RelationManagers;
+use App\Models\Apartment;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\FileUpload;
+
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\ImageColumn;
+
+
+
+
+
+class ApartmentResource extends Resource
+{
+    protected static ?string $model = Apartment::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name'),
+                RichEditor::make('description'),
+                FileUpload::make('thumbnail')
+
+
+
+
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')->sortable()->searchable(),
+                ImageColumn::make('thumbnail')
+
+
+
+
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListApartments::route('/'),
+            'create' => Pages\CreateApartment::route('/create'),
+            'edit' => Pages\EditApartment::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Resources/AttractionResource.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\AttractionResource\Pages;
+use App\Filament\Resources\AttractionResource\RelationManagers;
+use App\Models\Attraction;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Forms\Components\FileUpload;
+use Filament\Tables\Columns\TextColumn;
+
+
+
+class AttractionResource extends Resource
+{
+    protected static ?string $model = Attraction::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('title'),
+                Forms\Components\TextArea::make('content'),
+                FileUpload::make('thumbnail')
+
+
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('title')->sortable()->searchable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListAttractions::route('/'),
+            'create' => Pages\CreateAttraction::route('/create'),
+            'edit' => Pages\EditAttraction::route('/{record}/edit'),
+        ];
+    }
+}
